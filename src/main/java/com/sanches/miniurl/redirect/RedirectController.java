ea89f0ab53b9371fc78c8ca7f342ac3444566227
Gabriel Sanches <gabriel.c.sanches@outlook.com>
2025-04-06 16:05:45 -0400

    Adding RedirectController with different redirect approaches

---
+++ src/main/java/com/sanches/miniurl/redirect/RedirectController.java
@@ -0,0 +1,37 @@
+package com.sanches.miniurl.redirect;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class RedirectController {
+
+    @GetMapping("/to")
+    public String toMapping() {
+//        return "redirect:/";
+        return "hi there\n";
+    }
+
+    @GetMapping("/1")
+    public ResponseEntity<String> option1() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, "https://google.com");
+        return new ResponseEntity<>(headers, HttpStatus.TEMPORARY_REDIRECT);
+    }
+
+    @GetMapping("/2")
+    public String option2() {
+        return "redirect:https://google.com";
+    }
+
+    @GetMapping("/3")
+    public RedirectView option3() {
+        RedirectView redirect = new RedirectView("https://google.com");
+        redirect.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+        return redirect;
+    }
+}
