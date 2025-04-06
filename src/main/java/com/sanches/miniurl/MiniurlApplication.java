f3a61c4c6bcc9af1b565e8b12b6193518f6c8ed2
Gabriel Sanches <gabriel.c.sanches@outlook.com>
2025-04-06 15:33:17 -0400

    Initial commit

---
+++ src/main/java/com/sanches/miniurl/MiniurlApplication.java
@@ -0,0 +1,13 @@
+package com.sanches.miniurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniurlApplication.class, args);
+	}
+
+}
