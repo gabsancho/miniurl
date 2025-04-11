a4b6b6358ac0e1e47ee4d6ab625872400d16d06d
Gabriel Sanches <gabriel.c.sanches@outlook.com>
2025-04-10 20:07:42 -0400

    feat: Providing different input types for registering redirects

--- src/main/java/com/sanches/miniurl/adapter/rest/RedirectController.java
+++ src/main/java/com/sanches/miniurl/adapter/rest/RedirectController.java
@@ -2,6 +2,7 @@ package com.sanches.miniurl.adapter.rest;
 
 import com.sanches.miniurl.adapter.model.RegisterInput;
 import com.sanches.miniurl.application.GetRedirection;
+import com.sanches.miniurl.application.RegisterRedirection;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -10,50 +11,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
 public class RedirectController {
     private static final Log log = LogFactory.getLog(RedirectController.class);
     private final GetRedirection getRedirection;
-
-    @GetMapping("/1")
-    public ResponseEntity<String> option1() {
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "https://google.com");
-        return new ResponseEntity<>(headers, HttpStatus.TEMPORARY_REDIRECT);
-    }
-
-    @GetMapping("/2")
-    public String option2() {
-        return "redirect:https://google.com";
-    }
-
-    @GetMapping("/3")
-    public RedirectView option3() {
-        RedirectView redirect = new RedirectView("https://google.com");
-        redirect.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
-        return redirect;
-    }
+    private final RegisterRedirection registerRedirection;
 
     @GetMapping("/re/{code}")
-    public RedirectView option4(@PathVariable String code) {
-        String target = getRedirection.execute(code);
-        RedirectView redirect = new RedirectView(target);
-        redirect.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
-        return redirect;
+    public ResponseEntity<?> option4(@PathVariable String code) {
+        try {
+            String target = getRedirection.execute(code);
+            return ResponseEntity
+                    .status(HttpStatus.MOVED_PERMANENTLY)
+                    .header(HttpHeaders.LOCATION, target)
+                    .build();
+        } catch (RuntimeException ignored) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .build();
+        }
+    }
+
+    private String registerBase(String target) {
+        log.info("Registering redirect to " + target);
+        return registerRedirection.registerRedirection(target).target();
     }
 
     @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public String register(@RequestBody RegisterInput input) {
-        log.info("Registering redirect to " + input.target());
-        return "";
+        return registerBase(input.target());
     }
 
-    @PostMapping(value = "/register2", consumes = MediaType.TEXT_PLAIN_VALUE)
+    @PostMapping(value = "/register", consumes = MediaType.TEXT_PLAIN_VALUE)
     public String register(@RequestBody String target) {
-        log.info("Registering redirect to " + target);
-        return "";
+        return registerBase(target);
     }
 }
